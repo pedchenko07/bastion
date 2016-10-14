161e6edb871ac95ab2446c0b90c8d6029066f297
Roman <pedchenko07@gmail.com>
2016-10-14 18:18:56 +0300

    modify user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->tinyinteger('role_id')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
