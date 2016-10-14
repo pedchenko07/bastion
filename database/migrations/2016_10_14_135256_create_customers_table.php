575eafa70e5a44db92abf9546633102a18f953a0
Roman <pedchenko07@gmail.com>
2016-10-14 17:22:47 +0300

    create Models/Customer and create Models/Slider

---
+++ database/migrations/2016_10_14_135256_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default(null)->nullable();
+            $table->string('email')->default(null)->nullable();
+            $table->integer('phone')->default(0)->nullable();
+            $table->string('city')->default(null)->nullable();
+            $table->string('adres')->default(null)->nullable();
+            $table->string('comment')->default(null)->nullable();
+            $table->tinyInteger('payment_type')->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customers');
+    }
+}
---
+++ database/migrations/2016_10_14_140729_create_sliders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default(null)->nullable();
+            $table->tinyInteger('status')->default(0)->nullable();
+            $table->string('location')->default(null)->nullable();
+            $table->string('type')->default(null)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sliders');
+    }
+}
