4a6ac774502c02477d77060ed938cd1afc06f22b
Roman <pedchenko07@gmail.com>
2016-10-14 17:52:11 +0300

    create Models/Review and migration

---
+++ database/migrations/2016_10_14_144345_create_rewievs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRewievsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rewievs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default(null)->nullable();
+            $table->string('email')->default(null)->nullable();
+            $table->text('text')->default(null)->nullable();
+            $table->integer('product_id')->default(0);
+            $table->date('date');
+            $table->tinyInteger('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rewievs');
+    }
+}
