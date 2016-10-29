486ec991c94988d36afa04ac4a594b5da2c1b762
Maksym Grom <MailMaximGrom@gmail.com>
2016-10-29 23:59:43 +0300

    admin setting page is ready for production, but need change eng msg to ru msg

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     protected $table = 'setting';
+
+    public static function getFirst()
+    {
+        return self::first();
+    }
+
+    public static function getFirstName()
+    {
+        return self::select('name_shop')->first();
+    }
+
+    public static function updateSettings(array $data)
+    {
+        return self::whereId(self::getIdSettings()->id)->update($data);
+    }
+
+    public static function getIdSettings()
+    {
+        return self::select('id')->first();
+    }
 }
--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -14,46 +14,54 @@
                     <th class="str_action">Действие</th>
                 </tr>
                 <tr>
-                    <td class="name_page">NAME_SHOP</td>
-                    <td><a href="?view=edit_settings&amp;setting_id=setting_id" class="edit-button">Редактировать</a></td>
+                    <td class="name_page">{{$settings->name_shop}}</td>
+                    <td><a href="{{route('settings.edit')}}" class="edit-button">Редактировать</a></td>
                 </tr>
             </table>
         </div>
 
         <div style="border-bottom:1px solid #9A9B9B; border-top:1px solid #9A9B9B; padding-top:20px;">
             <h2>Редактирование способов доставки</h2>
-            <a href="?view=add_dostavka" class="admin-button">Добавить способ доставки</a>
+            <a href="{{route('settings.delivery')}}" class="admin-button">Добавить способ доставки</a>
             <table class="tabl" cellspacing="1">
                 <tr>
                     <th class="number">№</th>
                     <th class="str_name">Способ доставки</th>
                     <th class="str_action">Действие</th>
                 </tr>
+                @for($i = 0, $count = count($deliveryList); $i < $count; $i++)
                 <tr>
-                    <td>NUM</td>
-                    <td class="name_page">NAME</td>
-                    <td><a href="?view=edit_dostavka&amp;dostavka_id=DOUSTAVKA_ID" class="edit-button">изменить</a>&nbsp; | &nbsp;<a href="?view=del_dostavka&amp;dostavka_id=dostavka_id" class="zakaz-del">удалить</a></td>
+                    <td>{{$i+1}}</td>
+                    <td class="name_page">{{$deliveryList[$i]->name}}</td>
+                    <td><a href="{{route('settings.delivery.edit', ['id' => $deliveryList[$i]->id])}}"
+                           class="edit-button">изменить</a>&nbsp; | &nbsp;
+                        <a href="{{route('settings.delivery.delete', ['id' => $deliveryList[$i]->id])}}"
+                                                                             class="zakaz-del">удалить</a></td>
                 </tr>
+                @endfor
             </table>
-            <a href="?view=add_dostavka" class="admin-button">Добавить способ доставки</a>
+            <a href="{{route('settings.delivery')}}" class="admin-button">Добавить способ доставки</a>
         </div>
 
         <div style="margin-top:20px; border-bottom:1px solid #9A9B9B;">
             <h2>Редактирование способов оплаты</h2>
-            <a href="?view=add_oplata" class="admin-button">Добавить способ оплаты</a>
+            <a href="{{route('settings.oplata')}}" class="admin-button">Добавить способ оплаты</a>
             <table class="tabl" cellspacing="1">
                 <tr>
                     <th class="number">№</th>
                     <th class="str_name">Способ оплаты</th>
                     <th class="str_action">Действие</th>
                 </tr>
+                @for($i = 0, $count = count($oplataList); $i < $count; $i++)
                 <tr>
-                    <td>NUMBER</td>
-                    <td class="name_page">NAME</td>
-                    <td><a href="?view=edit_oplata&amp;oplata_id=ID" class="edit-button">изменить</a>&nbsp; | &nbsp;<a href="?view=del_oplata&amp;oplata_id=OPLATA_ID" class="zakaz-del">удалить</a></td>
+                    <td>{{$i+1}}</td>
+                    <td class="name_page">{{$oplataList[$i]->name}}</td>
+                    <td><a href="{{route('settings.oplata.edit', ['id' => $oplataList[$i]->id])}}" class="edit-button">изменить</a>&nbsp; | &nbsp;
+                        <a href="{{route('settings.oplata.delete', ['id' => $oplataList[$i]->id])}}" class="zakaz-del">удалить</a></td>
                 </tr>
+                @endfor
             </table>
-            <a href="?view=add_oplata" class="admin-button">Добавить способ оплаты</a>
+            <a href="{{route('settings.oplata')}}" class="admin-button">Добавить способ оплаты</a>
         </div>
 
     </div> <!-- .content -->
